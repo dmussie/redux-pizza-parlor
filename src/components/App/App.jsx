1c454bb2ddd5a8d13503b429d42f1536d899e7e6
Chris Hess <chrishessmusic@gmail.com>
2021-09-23 13:24:17 -0500

    created a fetchPizzaList

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,8 +3,26 @@ import axios from 'axios';
 import './App.css';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import PizzaList from '../PizzaList/PizzaList';
+import {useDispatch} from 'react-redux';
+import { useEffect } from 'react';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetchPizzaList();
+  },[]);
+  
+  const fetchPizzaList = () => {
+    axios.get('/api/pizza').then(response => {
+      console.log(response.data);
+      const action = {type: 'SET_PIZZA_LIST', payload: response.data}
+      dispatch(action);
+    });
+  }
+
+
+
 
   return (
     <div className='App'>
