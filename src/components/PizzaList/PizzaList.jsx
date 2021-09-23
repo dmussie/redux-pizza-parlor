402d3dc2a6e9d3469f884b8661ab010d30cb0aa4
Chris Hess <chrishessmusic@gmail.com>
2021-09-23 16:18:40 -0500

    created working add to cart button

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,19 +1,20 @@
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 import PizzaListItem from './PizzaListItem';
+import './PizzaList.css';
 
 function PizzaList() {
     console.log('in PizzaList');
     const reduxStore = useSelector(store => store);
     console.log(reduxStore.pizzaReducer);
     return (
-        <section>
+        <section className='pizza-list'>
             <ul>
-            {reduxStore.pizzaReducer.map((pizza) => {
-                return <PizzaListItem key={pizza.id} pizza={pizza}/>
-            }
-            
-                
-            )}
+                {reduxStore.pizzaReducer.map((pizza) => {
+                    return <PizzaListItem key={pizza.id} pizza={pizza} />
+                }
+
+
+                )}
             </ul>
         </section>
     );
--- src/components/PizzaList/PizzaListItem.jsx
+++ src/components/PizzaList/PizzaListItem.jsx
@@ -1,14 +1,28 @@
-function PizzaListItem({pizza}) {
-    return(
+import { useDispatch } from 'react-redux';
+import './PizzaList.css';
+
+function PizzaListItem({ pizza }) {
+    const dispatch = useDispatch();
+
+    const handleAddToCart = (params) => {
+        console.log('pizza to add', pizza.id);
+        const action = ({type: 'ADD_TO_CART', payload: pizza});
+        dispatch(action);
+        alert(pizza.name + ' Pizza added!')
+    }
+
+    return (
         <div className="pizza-box">
-                    <img className="pizza-img" src={pizza.image_path}/>
-                    <br />
-                    {pizza.name} 
-                    <br />
-                    {pizza.description} 
-                    <br />
-                    {pizza.price}
-                    </div>
+            <img className="pizza-img" src={pizza.image_path} />
+            <br />
+            {pizza.name}
+            <br />
+            {pizza.description}
+            <br />
+            {pizza.price}
+            <br />
+            <button onClick={handleAddToCart}>Add To Cart</button>
+        </div>
     );
 }
 
